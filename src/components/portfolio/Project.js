b12c0245a38ab5984f0a2bc4ed32ba48af58652d
510891328 <as510891328@gmail.com>
2021-01-30 18:27:21 -0500

    styled project detail

--- src/components/portfolio/Project.js
+++ src/components/portfolio/Project.js
@@ -1,18 +1,20 @@
+import '../../styles/Project.scss';
+
 export default function Project(props) {
   console.log(props.project);
   return (
-    <div>
+    <div className="project">
       <div className="gits">
-        <div> {'</>'}Front-End </div>
-        <div> {'</>'}Back-End </div>
-        <div>Demo</div>
-        <div>Site</div>
+        <div> <a href={props.project.github.frontEnd} rel="noreferrer" target="_blank">{'</>'}Front-End</a> </div>
+        <div> <a href={props.project.github.frontEnd} rel="noreferrer" target="_blank">{'</>'}Back-End</a> </div>
+        <div> <a href={props.project.github.frontEnd} rel="noreferrer" target="_blank">Video</a></div>
+        <div> <a href={props.project.github.frontEnd} rel="noreferrer" target="_blank">Site</a></div>
       </div>
-      <div>
+      <div className="project-image">
         <img src={props.project.image} alt="source"/>
       </div>
       <div className="dev-langs">
-        {props.project.language.map(lan => <div>{lan}</div>)}
+        {props.project.language.map(lan => <div>#{lan}</div>)}
       </div>
     </div>
   )
