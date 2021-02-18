7f81aa8cb3b3d12aef80800f571e663ec2261c52
510891328 <as510891328@gmail.com>
2021-02-18 18:52:27 -0500

    Add skill

--- src/data.js
+++ src/data.js
@@ -8,7 +8,7 @@ export const data = {
   nickName: "Eddy",
   programmingLanguage: {
     primary: ["JavaScript", "React.js", "Redux.js", "Ruby on Rail"],
-    others: ["MVC", "Python", "Php", "SQL", "FireBase", "NoSQL", "PostSQL"]
+    others: ["Object Oriented Design", "MVC", "Python", "Php", "SQL", "FireBase", "NoSQL", "PostSQL"]
   },
   language: ["English", "Mandarin"],
   about: "I am a Junior Software Engineer and I love to learn more about technology. I graduated with a Computer Information System major from Baruch with main focuses on the system design, then went to Flatiron to improve my skill with React.js, Ruby on Rails, and JavaScript. Developing apps and figuring out the logic is fascinating. ",
